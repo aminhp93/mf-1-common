555132ad4b2b1f1324b765b5df52ff503691c859
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-11-17 08:38:40 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    if (!options.isServer) {
+      //config.cache=false
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "common",
+          remotes: {},
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./title": "./pages/index.tsx",
+          },
+          shared: {},
+          extraOptions: {},
+        })
+      );
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
