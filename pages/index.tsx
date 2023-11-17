7b3b45d898fcdcb028b01d6d7f6a5acfa60bd558
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-11-17 09:38:10 +0100

    Add MUI package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Common 3</div>;
+// export default function Home() {
+//   return <div>Common 3</div>;
+// }
+
+import * as React from "react";
+// import Button from "@mui/material/Button";
+import SimpleDialogDemo from "@/components/SimpleDialogDemo";
+import DataGridDemo from "@/components/DataGridDemo";
+import DateTimeRangeDemo from "@/components/DateTimeRangeDemo";
+
+export default function ButtonUsage() {
+  return (
+    <>
+      <SimpleDialogDemo />;
+      <DataGridDemo />;
+      <DateTimeRangeDemo />;
+    </>
+  );
 }
